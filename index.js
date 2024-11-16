e074222895facbd5a12aa1d9e58f2ef27c5c90aa
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-11-16 10:09:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const numero = 3;
+
+// if (numero < 3) {
+//     console.log("Il numero è < di 3")
+// } else {
+//     console.log("Il numero non è < di 3");
+// }
+
+// Per eseguirlo dal terminale CTRL+ò, basta scrivere "node index.js"
+
+console.log("Dirname: ", __dirname); // Restituisce la directory senza il nome del file
+console.log("Filename: ", __filename); // Restituice la directory e il nome del file
+console.log("Process: ", process); // Ci permette di capire se stiamo lavorando in locale oppure in un server
