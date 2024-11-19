7c8c2f205ba19403bc1e7a123f2def34b4252b5b
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-11-19 10:59:27 +0100

    Aggiunti gli streams, e aggiunta l'estensione ai moduliBuiltIn

--- index.js
+++ index.js
@@ -240,4 +240,27 @@ Fine esempio event emitters */
 //
 //
 //
-// Stream
+// Streams
+// Sono dei flussi di dati
+// Consiglio altamente di guardare i file che iniziano con 5_
+const { createReadStream, readFileSync, writeFileSync } = require("fs");
+
+/*
+// Qui scrivo 10000 righe
+for (let i = 1; i < 10001; i++) {
+	writeFileSync("./5_cartella/fileGrande.txt", `Ciao riga ${i}\n`, {
+		flag: "a",
+	});
+}
+*/
+
+// Così ottengo il buffer, e mi dirà le dimensioni del file
+const fileGrande = readFileSync("./5_cartella/fileGrande.txt");
+console.log(fileGrande);
+
+const stream = createReadStream("./5_cartella/fileGrande.txt");
+// qui non possiamo scegliere noi il nome, sarà sempre data
+stream.on("data", (result) => {
+	console.log(result); // Così otteniamo i file in chunk al posto di avere tutto in una volta sola, viene spacchettato si potrebbe dire
+});
+console.log(fileGrande);
