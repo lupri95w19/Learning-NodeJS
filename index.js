d4c6eec58562483be45dcb5726f070e946fa8113
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-11-20 11:12:49 +0100

    Aggiunti i route param

--- index.js
+++ index.js
@@ -481,3 +481,91 @@ app.get("/", (req, res) => {
 app.listen(3000);
 
 Fine esempio usare i JSON */
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// Route Param
+/* Inizio esempio route param
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Qui si prende da un altro file
+const { persone } = require("./7_cartella/7_persone");
+
+app.get("/", (req, res) => {
+	res.send("<h1>Homepage</h1> <a href='./persone'>Vai a persone</a>");
+});
+
+app.get("/persone", (req, res) => {
+	const NuovePersone = persone.map((persona) => {
+		const { nome, cognome, eta } = persona;
+		// Adesso sparirà l'età
+		return { nome, cognome };
+	});
+
+	// res.sendFile("7_persone.js", { root: path.join(__dirname, "7_cartella") });
+	res.json(NuovePersone);
+});
+
+// Manuale
+// app.get("/persone/1", (req, res) => {
+// 	const persona = persone.find((persona) => {
+// 		persona.id === 1;
+// 		res.json(persona);
+// 	});
+// });
+
+// Automatico
+app.get("/persone/:id", (req, res) => {
+	console.log(req.params);
+	// Perché ha lo stesso nome posso fare così
+	const { id } = req.params;
+	// Oppure quando il nome è diverso
+	// const id = req.params.id;
+
+	const persona = persone.find((persona) => {
+		return persona.id === parseInt(id);
+	});
+
+	if (!persona) {
+		// return res.status(404).send("Prodotto non trovato");
+		return res.status(404).json({ messaggio: "Non trovato", error: "404" });
+	}
+
+	res.json(persona);
+});
+
+app.listen(3000);
+
+Fine esempio esempio route param */
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// Qui
