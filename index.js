c351494d5080e27965038438cd7d315aee233c98
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-11-20 09:44:44 +0100

    Aggiunti i file statici con express

--- index.js
+++ index.js
@@ -312,3 +312,39 @@ app.all("*", (req, res) => {
 app.listen(3000);
 
 fine esempio introduzione a Express */
+//
+//
+//
+//
+//
+//
+//
+//
+// Aggiungere file statici
+// creare cartella public
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+// app.get("/", function (req, res) {
+// 	res.send("Hello World, introduzione a Express");
+// });
+
+app.get("/", (req, res) => {
+	res.sendFile("home.html", { root: __dirname + "/public" });
+});
+
+app.get("/about", (req, res) => {
+	res.sendFile("about.html", { root: __dirname + "/public" });
+});
+
+app.get("/contacts", (req, res) => {
+	res.sendFile("contacts.html", { root: __dirname + "/public" });
+});
+
+// qualsiasi cosa che non è stata gestita sopra, avrà questo
+app.all("*", (req, res) => {
+	res.sendFile("404.html", { root: __dirname + "/public" });
+});
+
+app.listen(3000);
